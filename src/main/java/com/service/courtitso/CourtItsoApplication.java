28cd99b944a1d6381d044dcfdb742864db2af658
changmoo <public.ellerymoon@gmail.com>
2023-08-08 22:19:30 +0900

    Initial commit

---
+++ src/main/java/com/service/courtitso/CourtItsoApplication.java
@@ -0,0 +1,13 @@
+package com.service.courtitso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtItsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourtItsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/courtitso/CourtItsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.courtitso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtItsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
